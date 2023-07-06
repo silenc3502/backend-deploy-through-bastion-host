e637a593b0030a753fb01c9a46a2151bc12648c2
Sanghoon Lee <gcccompil3r@gmail.com>
2023-07-06 17:49:20 +0900

    init: ready to deploy through bastion host

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,18 +1,23 @@
 package com.example.demo.config;
 
+import com.example.demo.utility.PropertyUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
+
+    final private PropertyUtil propertyUtil;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        final String ALLOWED_ORIGINS = propertyUtil.getProperty("allowed_origins");
+
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8081")
-                .allowedOrigins("http://localhost:8081")
-                .allowedOrigins("http://15.164.174.190")
+                .allowedOrigins(ALLOWED_ORIGINS)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
